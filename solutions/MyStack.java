e0da50454c20c385c470563433e72bee5328f82b
Josh Kelle <joshua.a.kelle@gmail.com>
2015-08-07 21:47:48 -0700

    MyStack solution

---
+++ solutions/MyStack.java
@@ -0,0 +1,82 @@
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+
+public class MyStack<E> implements MyStackInterface<E> {
+
+	private ArrayList<E> arr;
+
+	/**
+	 * Constructor
+	 */
+	public MyStack() {
+		arr = new ArrayList<E>();
+	}
+
+	/**
+	 * Constructor
+	 * @param arr - array of elements to be added to this stack. arr[0] is the first element to be pushed.
+	 */
+	public MyStack(E[] arr) {
+		for(E element : arr) {
+			this.arr.add(element);
+		}
+	}
+
+	/**
+	 * Tests if this stack is empty.
+	 * @return true if and only if this stack contains no items; false otherwise.
+	 *
+	 * O(1)
+	 */
+	public boolean isEmpty() {
+		return arr.size() == 0;
+	}
+
+	/**
+	 * Looks at the object at the top of this stack without removing it from the stack.
+	 * @return the object at the top of this stack.
+	 * @throw EmptyStackException if this stack is empty.
+	 *
+	 * O(1)
+	 */
+	public E peek() {
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		}
+
+		return arr.get(arr.size() -1);
+	}
+
+	/**
+	 * Removes and returns the object at the top of this stack.
+	 * @return the object at the top of this stack.
+	 * @throw EmptyStackException if this stack is empty.
+	 *
+	 * O(1)
+	 */
+	public E pop() {
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		}
+
+		E element = peek();
+		arr.remove(arr.size() -1);
+
+		return element;
+	}
+
+	/**
+	 * Pushes an item onto the top of this stack.
+	 * @param item - the item to be pushed onto this stack.
+	 * @return the item argument.
+	 *
+	 * O(1)
+	 */
+	public E push(E item) {
+		arr.add(item);
+		return peek();
+	}
+
+}
